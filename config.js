fba50c77b0193b951f2ef2071bdb7d607ac74c2a
Emalitownke1 <emalitownke1@gmail.com>
2025-07-02 01:10:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // USE ENVIRONMENT VARIABLES FOR EMAIL AND PASSWORD INSTEAD.
 module.exports = {
   PREFIX: process.env.PREFIX || "IRON-M4N×",
-  EMAIL: process.env.EMAIL || "ironman@onlyfans.com",
-  PASS: process.env.PASS || "Kimi da yo kimi nandayo..",
-  MESSAGE: process.env.MESSAGE || "Thank you for using our bot dont share your session id blah blah .etc",
-  IMAGE: process.env.IMAGE || "https://cdn.ironman.my.id/i/2iceb4.jpeg",
+  EMAIL: process.env.EMAIL || "emalitownke1@gmail.com",
+  PASS: process.env.PASS || "password ndio hii",
+  MESSAGE: process.env.MESSAGE || "Thank you for using Ttekker-md bot dont share your session id blah blah .etc",
+  IMAGE: process.env.IMAGE || "https://i.postimg.cc/0Nrf4fxL/IMG-20250419-131400-618.jpg",
 }
